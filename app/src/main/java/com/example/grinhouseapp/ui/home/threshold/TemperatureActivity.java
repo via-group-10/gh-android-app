5249bb6d153164673357ce8a521aa2add4b0b459
littlelenka3 <56080011+littlelenka3@users.noreply.github.com>
2021-04-21 20:12:52 +0200

    small design changes

--- app/src/main/java/com/example/grinhouseapp/ui/home/threshold/TemperatureActivity.java
+++ app/src/main/java/com/example/grinhouseapp/ui/home/threshold/TemperatureActivity.java
@@ -5,20 +5,24 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.grinhouseapp.R;
 
 public class TemperatureActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperature);
-
         Intent bundle = getIntent();
         Toolbar toolbar = findViewById(R.id.up_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
     }
 }
--- app/src/main/java/com/example/grinhouseapp/ui/home/threshold/TemperatureViewModel.java
+++ app/src/main/java/com/example/grinhouseapp/ui/home/threshold/TemperatureViewModel.java
@@ -4,4 +4,6 @@ import androidx.lifecycle.ViewModel;
 
 public class TemperatureViewModel extends ViewModel {
 
+
+
 }
