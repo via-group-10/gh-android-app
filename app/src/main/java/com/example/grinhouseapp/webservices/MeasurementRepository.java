341a037cba68c06e3c6d38b3d0aabd1ba7e787d0
AntonJanto <anton.janto@gmail.com>
2021-04-25 22:21:01 +0200

    all measurements api

--- app/src/main/java/com/example/grinhouseapp/webservices/MeasurementRepository.java
+++ app/src/main/java/com/example/grinhouseapp/webservices/MeasurementRepository.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -34,18 +36,25 @@ public class MeasurementRepository {
     public void  setMeasurement(MeasurementType type)
     {
         MeasurementApi measurementApi = ServiceGenerator.getMeasurementApi();
-        Call<MeasurementResponse> call = measurementApi.getMeasurement(type);
-        call.enqueue(new Callback<MeasurementResponse>() {
+        Call<List<MeasurementResponse>> call = measurementApi.getAllMeasurements();
+        call.enqueue(new Callback<List<MeasurementResponse>>() {
             @EverythingIsNonNull
             @Override
-            public void onResponse(Call<MeasurementResponse> call, Response<MeasurementResponse> response) {
-                if(response.isSuccessful())
-                    measurementMutableLiveData.setValue(response.body().getMeasurement());
+            public void onResponse(Call<List<MeasurementResponse>> call, Response<List<MeasurementResponse>> response) {
+                if(response.isSuccessful()) {
+                    List<MeasurementResponse> measurementResponses = response.body();
+                    for (MeasurementResponse measurementResponse : measurementResponses) {
+                        if (measurementResponse.getMeasurement().getType() == MeasurementType.TEMPERATURE)
+                        {
+                            measurementMutableLiveData.setValue(measurementResponse.getMeasurement());
+                        }
+                    }
+                }
             }
 
             @EverythingIsNonNull
             @Override
-            public void onFailure(Call<MeasurementResponse> call, Throwable t) {
+            public void onFailure(Call<List<MeasurementResponse>> call, Throwable t) {
                 Log.i("Retrofit", "Something went wrong");
             }
         });
