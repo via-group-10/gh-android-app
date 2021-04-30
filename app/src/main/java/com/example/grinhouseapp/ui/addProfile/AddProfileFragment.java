0bb33af5a0229fc96729fa604e5bfe64ed817201
Patrik <patulda111@gmail.com>
2021-04-30 15:39:16 +0200

    add profile

---
+++ app/src/main/java/com/example/grinhouseapp/ui/addProfile/AddProfileFragment.java
@@ -0,0 +1,51 @@
+package com.example.grinhouseapp.ui.addProfile;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.grinhouseapp.R;
+
+public class AddProfileFragment extends Fragment {
+
+    private Button cancelButton;
+    private Button saveButton;
+
+    public static AddProfileFragment newInstance() {
+        return new AddProfileFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_add_profile, container, false);
+        cancelButton = view.findViewById(R.id.button_cancel);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(getView()).navigate(R.id.navigation_profile);
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // TODO: Use the ViewModel
+    }
+
+}
