803bada060ecb1adbe62b53f8a810ec01632d3cd
Niczk <popal.nicolas@gmail.com>
2021-05-24 16:30:19 +0200

    Graph done :shit:

--- app/src/main/java/com/example/grinhouseapp/ui/graph/GraphFragment.java
+++ app/src/main/java/com/example/grinhouseapp/ui/graph/GraphFragment.java
@@ -1,32 +1,28 @@
 package com.example.grinhouseapp.ui.graph;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.RadioButton;
 
 import com.example.grinhouseapp.R;
-import com.example.grinhouseapp.model.Measurement;
 import com.example.grinhouseapp.model.MeasurementType;
-import com.example.grinhouseapp.ui.data.DataViewModel;
-import com.example.grinhouseapp.ui.filter.FilterViewModel;
-import com.github.mikephil.charting.data.Entry;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 
 public class GraphFragment extends AppCompatActivity {
 
     private GraphViewModel viewModel;
     LineGraphSeries<DataPoint> series;
+    RadioButton dailyButton, weeklyButton, monthlyButton;
+    GraphView graph;
+    int category;
+    String filter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,38 +31,79 @@ public class GraphFragment extends AppCompatActivity {
         setContentView(R.layout.graph_fragment);
 
         Intent intent = getIntent();
-        GraphView graph = (GraphView) findViewById(R.id.graph);
+        graph = (GraphView) findViewById(R.id.graph);
         viewModel = new ViewModelProvider(this).get(GraphViewModel.class);
 
         /** TEMPERATURE = 0 ; HUMIDITY = 1 ; CO2 = 2 **/
-        int category = intent.getIntExtra("measurement", 0);
-        /*switch (category)
-        {
-            case 0:
-               break;
-        }*/
+        category = intent.getIntExtra("measurement", 0);
 
-        viewModel.setMeasurementRepository(MeasurementType.temperature, "monthly");
-        viewModel.getTemperatureGraphMeasurement().observe(this, measurements -> {
-            Log.i("Size", measurements.size()+"");
-            DataPoint[] dataPoints = new DataPoint[measurements.size()];
-            for (int i = 0;i<measurements.size();i++){
-                dataPoints[i] = new DataPoint(new Date(measurements.get(measurements.size()-1-i).getMeasurementDateTime().getTime()), measurements.get(measurements.size()-1-i).getMeasurementValue());
-                Log.i("Measurement" + i, dataPoints[i].getX() + " - " +dataPoints[i].getY());
-            }
-            series = new LineGraphSeries<>(dataPoints);
-            graph.addSeries(series);
+        filter = "monthly";
+        drawGraph(filter);
+
+        dailyButton = findViewById(R.id.dailyButton);
+        weeklyButton = findViewById(R.id.weeklyButton);
+        monthlyButton = findViewById(R.id.monthlyButton);
+
+        dailyButton.setOnClickListener(v -> {
+            filter = "daily";
+            drawGraph(filter);
         });
 
+        weeklyButton.setOnClickListener(v -> {
+            filter = "weekly";
+            drawGraph(filter);
+        });
 
+        monthlyButton.setOnClickListener(v -> {
+            filter = "monthly";
+            drawGraph(filter);
+        });
 
-
+        drawGraph(filter);
     }
 
 
-    private String toDate(long timestamp) {
-        Date date = new Date(timestamp);
-        return new SimpleDateFormat("yyyy-MM-dd").format(date);
+    private void drawGraph(String filter)
+    {
+        switch (category)
+        {
+            case 0:
+                viewModel.setMeasurementRepository(MeasurementType.temperature, filter);
+                viewModel.getTemperatureGraphMeasurement().observe(this, measurements -> {
+                    graph.removeAllSeries();
+                    DataPoint[] dataPoints = new DataPoint[measurements.size()];
+                    for (int i = 0;i<measurements.size();i++){
+                        dataPoints[i] = new DataPoint(new Date(measurements.get(measurements.size()-1-i).getMeasurementDateTime().getTime()), measurements.get(measurements.size()-1-i).getMeasurementValue());
+                    }
+                    series = new LineGraphSeries<>(dataPoints);
+                    graph.addSeries(series);
+                });
+                break;
+            case 1:
+                viewModel.setMeasurementRepository(MeasurementType.humidity, filter);
+                viewModel.getTemperatureGraphMeasurement().observe(this, measurements -> {
+                    graph.removeAllSeries();
+                    DataPoint[] dataPoints = new DataPoint[measurements.size()];
+                    for (int i = 0;i<measurements.size();i++){
+                        dataPoints[i] = new DataPoint(new Date(measurements.get(measurements.size()-1-i).getMeasurementDateTime().getTime()), measurements.get(measurements.size()-1-i).getMeasurementValue());
+                    }
+                    series = new LineGraphSeries<>(dataPoints);
+                    graph.addSeries(series);
+                });
+                break;
+            case 2:
+                viewModel.setMeasurementRepository(MeasurementType.carbonDioxide, filter);
+                viewModel.getTemperatureGraphMeasurement().observe(this, measurements -> {
+                    graph.removeAllSeries();
+                    DataPoint[] dataPoints = new DataPoint[measurements.size()];
+                    for (int i = 0;i<measurements.size();i++){
+                        dataPoints[i] = new DataPoint(new Date(measurements.get(measurements.size()-1-i).getMeasurementDateTime().getTime()), measurements.get(measurements.size()-1-i).getMeasurementValue());
+                    }
+                    series = new LineGraphSeries<>(dataPoints);
+                    graph.addSeries(series);
+                });
+                break;
+        }
     }
 
 
